b26aa11dc147b73b805cd726d86aa45c39258329
Santiago Foti <santiago.foti1@gmail.com>
2022-10-23 00:31:06 -0300

    ultima actualizacion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca un precio \nRecuerde que los decimales van despues de una coma");
+        try{
+            double precio= scanner.nextDouble();
+            System.out.println("El precio con IVA es: "+getPriceIVA(precio));
+        }catch(InputMismatchException e){
+            e.printStackTrace();
+        }finally{
+            System.out.println("Cierre de recursos");
+
+        }
+
+
+    }
+
+
+    static double getPriceIVA(double precio) {
+
+        return precio*21/100+precio;
+
     }
 }
